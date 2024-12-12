d9f82a5173951c1f71cd712796d7fb2253dd36e0
Deb-sudo <55880042+Deb-sudo@users.noreply.github.com>
2024-12-12 16:02:32 +0530

    Create hi.py
    committing changes

---
+++ hi.py
@@ -0,0 +1,14 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask the user for their age
+age = int(input("How old are you? "))
+
+# Print out a greeting message with their name
+print("Hello, " + name + "!")
+
+# Calculate how old they will be in 5 years
+future_age = age + 5
+
+# Print out a message telling them how old they will be in 5 years
+print("In 5 years, you will be " + str(future_age) + " years old.")
